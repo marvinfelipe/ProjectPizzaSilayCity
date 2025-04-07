e6c8ed862a8dbbd5ac82cb07701828b1e061999a
MarvinFelipe <marvinfelipe888@gmail.com>
2025-04-08 00:02:46 +0800

    Revert "Add files via upload"
    This reverts commit f02bca1aa0c7dabe0a6c8ee9c2c43513f401f8e4.

--- src/utils/validators.js
+++
@@ -1,9 +0,0 @@
-// js/utils/validators.js
-export function validateOrder(order) {
-    return (
-        order.customerName &&
-        order.phoneNumber &&
-        order.orderType &&
-        order.pizzas.length > 0
-    );
-}
